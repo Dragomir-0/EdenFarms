2020060bf2c10e0de43f15044d9c9ebcf236f848
Dragomir-0 <39728866+Dragomir-0@users.noreply.github.com>
2020-05-26 22:21:27 +0200

    Create Create EdenFarmsDB.sql

---
+++ DataBase/Scripts/Create EdenFarmsDB.sql
@@ -0,0 +1,71 @@
+CREATE DATABASE EdenFarmsDB
+ON PRIMARY
+(
+NAME= BCLibraryDBData,
+FILENAME= '%Documents%\EdenFarmsDB\EdenFarmsDBData.mdf',
+SIZE= 20MB,
+FILEGROWTH= 10%,
+MAXSIZE= 10GB
+)
+LOG ON
+(
+NAME= BCLibraryDBLogs,
+FILENAME= '%Documents%\EdenFarmsDB\EdenFarmsDBLogs.ldf',
+SIZE= 20MB,
+FILEGROWTH= 10%,
+MAXSIZE= 10GB
+)
+GO
+USE EdenFarmsDB
+GO
+CREATE Table tblUser
+(
+UserID INT IDENTITY PRIMARY KEY,
+UserName VARCHAR(40) NOT NULL,
+VatIDNumber VARCHAR(10) NOT NULL,
+ContactNumber VARCHAR(10) NOT NULL,
+Email VARCHAR(40) NOT NULL,
+Address VARCHAR NOT NULL
+);
+GO 
+CREATE Table tblFarm
+(
+FarmID INT IDENTITY PRIMARY KEY,
+UserID INT FOREIGN KEY REFERENCES User_TBL(UserID),
+FarmName VARCHAR(40) NOT NULL,
+FarmLocation VARCHAR NOT NULL,
+LocationAlias VARCHAR(40)
+)
+GO
+CREATE Table tblPlantCategory
+(
+PlantCategoryID INT IDENTITY PRIMARY KEY,
+CategoryName VARCHAR(40) NOT NULL,
+CategoryDescription VARCHAR NOT NULL
+)
+GO
+CREATE Table tblPlant
+(
+PlantID INT IDENTITY PRIMARY KEY,
+PlantCategoryID INT FOREIGN KEY REFERENCES PlantCategory_TBL(PlantCategoryID),
+PlantName VARCHAR(40) DEFAULT 0,
+OptimalAmbientTemp DECIMAL DEFAULT 0,
+OptimalAmbientHumidity DECIMAL DEFAULT 0,
+OptimalAmbientLight VARCHAR(7) DEFAULT 0,
+OptimalInternalTemp DECIMAL DEFAULT 0,
+OptimalInternalHumidity DECIMAL DEFAULT 0,
+OptimalInternalLight VARCHAR(7) DEFAULT 0,
+OptimalInternalOxygen DECIMAL DEFAULT 0,
+OptimalNitrogen DECIMAL DEFAULT 0,
+OptimalPhosphorus DECIMAL DEFAULT 0,
+OptimalPotassium DECIMAL DEFAULT 0
+)
+GO
+CREATE Table tblPlots
+(
+PlotID INT IDENTITY PRIMARY KEY,
+FarmID INT FOREIGN KEY REFERENCES Farm_TBL(FarmID),
+PlantID INT FOREIGN KEY REFERENCES Plant_TBL(PlantID),
+UserOverride VARCHAR(40),
+PerformanceReview VARCHAR(50)
+)
