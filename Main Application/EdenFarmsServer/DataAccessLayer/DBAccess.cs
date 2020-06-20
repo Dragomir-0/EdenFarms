2651f90765e222365bd1180a68bf3f684bdefe25
PhilNotFromTheFuture <mccarterphilip@gmail.com>
2020-06-20 12:47:38 +0200

    Update DBAccess.cs
    Changed the select and delete methods to be used with any table. They are now universal. Insert and Update methods are a different story...

--- Main Application/EdenFarmsServer/DataAccessLayer/DBAccess.cs
+++ Main Application/EdenFarmsServer/DataAccessLayer/DBAccess.cs
@@ -20,21 +20,19 @@ namespace Data_Access_Layer
             connection.IntegratedSecurity = true; //the integrated security of the connection
         }
 
-        public DataTable ReadData(string tableName, string storedProcedureName) //a method to read any table from the database using a stored procedure
+        public DataTable ReadData(string storedProcedureName) //a method to read any table from the database using a stored procedure
         {
             SqlConnection conn = new SqlConnection(connection.ToString());
             DataTable output = new DataTable(); //this will be needed for storing the data from a database table
-            SqlDataReader rdr = null;
+            SqlDataAdapter sda = new SqlDataAdapter();
             try //to prevent a crash if it cannot connect
             {
                 conn.Open();
                 SqlCommand cmd = new SqlCommand(storedProcedureName, conn); //a command is generated using the stored procedude name and the table name
                 cmd.CommandType = CommandType.StoredProcedure; //letting the code know that it is a stored procedude
-                rdr = cmd.ExecuteReader(); //start reading
-                while (rdr.Read()) //while it is reading
-                {
-                    output.Load(rdr); //store what is read into the datatable
-                }
+                sda.SelectCommand = cmd;
+                sda.FillSchema(output, SchemaType.Source);
+                sda.Fill(output);
             }
             catch (SqlException e) //for if an error occurs
             {
@@ -84,20 +82,27 @@ namespace Data_Access_Layer
             }
         }
 
-        public void DeleteData(string whatToDelete, string tableName)
+        public void DeleteData(string storedProcedure, string variable, string valueForDeletion)
         {
             SqlConnection conn = new SqlConnection(connection.ToString());
             try
             {
-
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(storedProcedure, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue(variable, valueForDeletion);
+                cmd.ExecuteNonQuery();
             }
             catch (SqlException e)
             {
-
+                MessageBox.Show(e.Message);
             }
             finally
             {
-
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
             }
         }
     }
