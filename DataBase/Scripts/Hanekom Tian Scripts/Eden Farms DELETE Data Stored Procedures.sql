85cbe169ca41a040e2862b4f014c09e74bdeb5f8
Tian Hanekom <hanekom.tian@gmail.com>
2020-07-09 16:24:42 +0200

    Added Transactions to Eden Farms DELETE Stored Procedures

--- DataBase/Scripts/Hanekom Tian Scripts/Eden Farms DELETE Data Stored Procedures.sql
+++ DataBase/Scripts/Hanekom Tian Scripts/Eden Farms DELETE Data Stored Procedures.sql
@@ -8,9 +8,23 @@ CREATE PROCEDURE spDeleteFarm
 )
 AS
 BEGIN
-DELETE FROM tblFarm 
-WHERE FarmID=@FarmID
-END 
+	BEGIN TRY
+		BEGIN TRANSACTION
+
+			DELETE FROM tblPlots
+			WHERE FarmID = @FarmID 
+
+			DELETE FROM tblFarm 
+			WHERE FarmID=@FarmID
+
+		COMMIT
+		PRINT 'spDeleteFarm Transaction Successful'
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+		PRINT 'spDeleteFarm Transaction UNSUCCSESSFUL'
+	END CATCH
+END
 
 GO
 
@@ -20,8 +34,27 @@ CREATE PROCEDURE spDeletePlantCategory
 )
 AS
 BEGIN
-DELETE FROM tblPlantCategory 
-WHERE PlantCategoryID=@CategoryID
+	BEGIN TRY
+		BEGIN TRANSACTION
+
+			DELETE FROM tblPlots
+			WHERE PlantID = (SELECT PlantID
+							FROM tblPlant
+							WHERE PlantCategoryID=@CategoryID)
+
+			DELETE FROM tblPlant
+			WHERE PlantCategoryID=@CategoryID
+
+			DELETE FROM tblPlantCategory 
+			WHERE PlantCategoryID=@CategoryID
+
+		COMMIT
+		PRINT 'spDeletePlantCategory Transaction Successful'
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+		PRINT 'spDeletePlantCategory Transaction UNSUCCSESSFUL'
+	END CATCH
 END
 
 GO
@@ -32,8 +65,19 @@ CREATE PROCEDURE spDeletePlot
 )
 AS
 BEGIN
-DELETE FROM tblPlots 
-WHERE PlotID= @PlotID
+	BEGIN TRY
+		BEGIN TRANSACTION
+
+			DELETE FROM tblPlots 
+			WHERE PlotID= @PlotID
+
+		COMMIT
+		PRINT 'spDeletePlot Transaction Successful'
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+		PRINT 'spDeletePlot Transaction UNSUCCSESSFUL'
+	END CATCH
 END
 
 GO
@@ -44,8 +88,22 @@ CREATE PROCEDURE spDeletePlant
 )
 AS
 BEGIN
-DELETE FROM tblPlant 
-WHERE PlantID=@PlantID
+	BEGIN TRY
+		BEGIN TRANSACTION
+
+			DELETE FROM tblPlots
+			WHERE PlantID=@PlantID
+
+			DELETE FROM tblPlant 
+			WHERE PlantID=@PlantID
+
+		COMMIT
+		PRINT 'spDeletePlant Transaction Successful'
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+		PRINT 'spDeletePlant Transaction UNSUCCSESSFUL'
+	END CATCH
 END 
 
 GO
@@ -56,8 +114,27 @@ CREATE PROCEDURE spDeleteUser
 )
 AS
 BEGIN 
-DELETE FROM tblUser 
-WHERE UserID = @UserID
+	BEGIN TRY
+		BEGIN TRANSACTION
+
+			DELETE FROM tblPlots
+			WHERE FarmID = (SELECT FarmID
+							FROM tblFarm
+							WHERE UserID=@UserID)
+
+			DELETE FROM tblFarm
+			WHERE UserID=@UserID
+
+			DELETE FROM tblUser 
+			WHERE UserID = @UserID
+
+		COMMIT
+		PRINT 'spDeleteUser Transaction Successful'
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+		PRINT 'spDeleteUser Transaction UNSUCCSESSFUL'
+	END CATCH
 END
 
 GO
@@ -68,6 +145,17 @@ CREATE PROCEDURE spDeleteRole
 )
 AS
 BEGIN
-DELETE FROM tblRole
-WHERE RoleID = @RoleID
+	BEGIN TRY
+		BEGIN TRANSACTION
+
+			DELETE FROM tblRole
+			WHERE RoleID = @RoleID
+
+		COMMIT
+		PRINT 'spDeleteRole Transaction Successful'
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+		PRINT 'spDeleteRole Transaction UNSUCCSESSFUL'
+	END CATCH
 END
