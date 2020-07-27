42a14bdc9ed2cf875c95d4505e8ecefe68414af0
PhilNotFromTheFuture <mccarterphilip@gmail.com>
2020-07-27 13:23:28 +0200

    More Forms
    I've added buttons, labels and text boxes to most forms that we need.

--- Main Application/EdenFarmsServer/PresentationLayer/ClientForm.cs
+++ Main Application/EdenFarmsServer/PresentationLayer/ClientForm.cs
@@ -26,32 +26,44 @@ namespace PresentationLayer
 
         private void btnAddClient_Click(object sender, EventArgs e)
         {
-
+            AddClientForm adf = new AddClientForm();
+            adf.Show();
+            this.Hide();
         }
 
         private void btnEditClient_Click(object sender, EventArgs e)
         {
-
+            UpdateClientForm ucf = new UpdateClientForm();
+            ucf.Show();
+            this.Hide();
         }
 
         private void btnDeleteClient_Click(object sender, EventArgs e)
         {
-
+            DeleteClientForm dcf = new DeleteClientForm();
+            dcf.Show();
+            this.Hide();
         }
 
         private void btnAddPlot_Click(object sender, EventArgs e)
         {
-
+            AddPlotForm apf = new AddPlotForm();
+            apf.Show();
+            this.Hide();
         }
 
         private void btnEditPlot_Click(object sender, EventArgs e)
         {
-
+            UpdatePlotForm upf = new UpdatePlotForm();
+            upf.Show();
+            this.Hide();
         }
 
         private void btnDeletePlot_Click(object sender, EventArgs e)
         {
-
+            DeletePlotForm dpf = new DeletePlotForm();
+            dpf.Show();
+            this.Hide();
         }
     }
 }
---
+++ Main Application/EdenFarmsServer/PresentationLayer/UpdatePlotForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace PresentationLayer
+{
+    partial class UpdatePlotForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFarmID = new System.Windows.Forms.TextBox();
+            this.txtPlant = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtPerformance = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(226, 144);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(163, 41);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Update Plot";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(12, 144);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(163, 41);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtPerformance);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtPlant);
+            this.panel1.Controls.Add(this.txtFarmID);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(377, 126);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Farm ID";
+            // 
+            // txtFarmID
+            // 
+            this.txtFarmID.Location = new System.Drawing.Point(142, 12);
+            this.txtFarmID.Name = "txtFarmID";
+            this.txtFarmID.Size = new System.Drawing.Size(210, 20);
+            this.txtFarmID.TabIndex = 1;
+            // 
+            // txtPlant
+            // 
+            this.txtPlant.Location = new System.Drawing.Point(142, 38);
+            this.txtPlant.Name = "txtPlant";
+            this.txtPlant.Size = new System.Drawing.Size(210, 20);
+            this.txtPlant.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Plant";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "User Override";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Performance Review";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // txtPerformance
+            // 
+            this.txtPerformance.Location = new System.Drawing.Point(142, 90);
+            this.txtPerformance.Name = "txtPerformance";
+            this.txtPerformance.Size = new System.Drawing.Size(210, 20);
+            this.txtPerformance.TabIndex = 7;
+            // 
+            // UpdatePlotForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(403, 202);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnCancel);
+            this.Name = "UpdatePlotForm";
+            this.Text = "UpdatePlotForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtFarmID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPlant;
+        private System.Windows.Forms.TextBox txtPerformance;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
