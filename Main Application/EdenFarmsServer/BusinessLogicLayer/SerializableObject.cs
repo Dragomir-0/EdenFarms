94a1c44fbed1c2f4758fc8868c757f6fa35d90c4
Dragomir-0 <39728866+Dragomir-0@users.noreply.github.com>
2020-08-26 10:28:50 +0200

    Added File Server

---
+++ Main Application/EdenFarmsServer/BusinessLogicLayer/SerializableObject.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BusinessLogicLayer
+{
+    [Serializable]
+    public class SerializableObject
+    {
+        //Use to display farm target 
+        private string farm;
+        //used to Data
+        private List<string> plotData;
+        //requset conenction
+        private string connectionRequest;
+
+
+        /// <summary>
+        /// Used to send data to farm.
+        /// </summary>
+        /// <param name="farmPrm">Farm ID according to Database</param>
+        /// <param name="plotDataPrm">Data to send to farm</param>
+        public SerializableObject(string farmPrm, List<string> plotDataPrm)
+        {
+            Farm = farmPrm;
+            PlotData = plotDataPrm;
+        }
+
+        /// <summary>
+        /// Request update from Farm to Server
+        /// </summary>
+        /// <param name="farmPrm">Farm ID</param>
+        /// <param name="connectionRequestPrm">Connection data {WIP}</param>
+        public SerializableObject(string farmPrm, string connectionRequestPrm)
+        {
+            Farm = farmPrm;
+            ConnectionRequest = connectionRequestPrm;
+        }
+        
+        public string Farm { get => farm; set => farm = value; }
+        public List<string> PlotData { get => plotData; set => plotData = value; }
+        public string ConnectionRequest { get => connectionRequest; set => connectionRequest = value; }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.Farm + " " + this.ConnectionRequest;
+        }
+
+        public string ToConsoleString()
+        {
+            string listData = this.PlotData[0];
+            int lengthOfList = this.PlotData.Count;
+            for (int i = 1; i < lengthOfList; i++)
+            {
+                listData += ", " + this.PlotData[i];
+            }
+            return this.Farm + ":   " + listData;
+        }
+    }
+}
---
+++ Main Application/EdenFarmsServer/EdenFarmsServer/BLL/ServerObject.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using BusinessLogicLayer;
+using HelperLib;
+
+namespace EdenFarmsServer
+{
+    public class ServerObject
+    {
+        List<Socket> clients = new List<Socket>();
+        IPHostEntry host;
+        IPAddress iPAddress;
+        IPEndPoint endPoint;
+        Socket serverSocket;
+        Thread acceptingThread;
+        Thread recievingThread;
+
+        public ServerObject()
+        {
+            ServerInitialize();
+            StartServer();
+        }
+
+        public void ServerInitialize()
+        {
+            
+            host = Dns.Resolve("172.16.3.189"); //DNS
+            iPAddress = host.AddressList[3]; //0
+            endPoint = new IPEndPoint(iPAddress, ChangableVariables.portNumber);
+
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Server setting up............");
+            Thread.Sleep(1000);
+        }
+
+        public void StartServer()
+        {
+            serverSocket.Bind(endPoint);
+            serverSocket.Listen(10);
+
+            acceptingThread = new Thread(() => AllowConnections());
+            acceptingThread.Start();
+            Console.WriteLine("Server Started............");
+            Thread.Sleep(1000);
+        }
+
+        public void AllowConnections()
+        {
+            while (true)
+            {
+                Socket client = serverSocket.Accept();
+                clients.Add(client);
+                recievingThread = new Thread(() => RecieveClientData(client));
+                recievingThread.Start();
+                Console.WriteLine("Connection Found............");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void RecieveClientData(Socket client)
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+                client.Receive(buffer);
+
+                SerializableObject connectionRequest = (SerializableObject)buffer.BinaryDeserialization();
+
+                Console.WriteLine(connectionRequest.ToString());
+                List<string> tesData = new List<string> { "One", "Two", "Three" };
+                client.Send(new SerializableObject(connectionRequest.Farm, tesData).BinarySerialization());
+                
+            }
+        }
+
+        public void SendFileTest()
+        {
+
+        }
+
+        public void StopServer()
+        {
+        }
+    }
+}
