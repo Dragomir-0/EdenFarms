d5080ea26deb390cc4b3b3567b5fe5ede294f5c3
Tian Hanekom <hanekom.tian@gmail.com>
2020-09-05 16:54:55 +0200

    Create FileDirectoryWatcher.cs

---
+++ Main Application/EdenFarmsServer/EdenFarmsServer/BLL/FileDirectoryWatcher.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdenFarmsServer.BLL
+{
+    class FileDirectoryWatcher
+    {
+        private string dir = "";
+
+        private FileSystemWatcher watcher = new FileSystemWatcher();
+
+        public FileDirectoryWatcher()
+        {
+            Thread FDWThread = new Thread(getDirectory);
+            FDWThread.Start();
+        }
+
+        public void getDirectory()
+        {
+
+            bool loop = true;
+
+            while (loop == true)
+            {
+                //Console.WriteLine("Enter the directory to be monitored:");
+
+                string input = "D:\\Documents\\1.Check This";//Console.ReadLine();
+
+                try
+                {
+                    if (Directory.Exists(input))
+                    {
+                        //Console.WriteLine("Monitoring:" + input);
+                        dir = input;
+                        loop = false;
+                    }
+                    else
+                    {
+                        //Console.WriteLine("Directory does not exist...");
+                        throw new DirectoryNotFoundException();
+                    }
+                }
+                catch (DirectoryNotFoundException dnfe)
+                {
+
+                }
+            }
+            startWatching();
+        }
+
+        public void startWatching()
+        {
+            watcher.Path = dir;
+
+            watcher.Filter = "*.*";
+
+            watcher.Changed += OnChanged;
+            watcher.Created += OnChanged;
+            watcher.Deleted += OnChanged;
+            watcher.Renamed += OnRenamed;
+
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press 'q' to quit the sample.");
+            while (Console.Read() != 'q') ;
+
+        }
+
+        public void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            sendInfo($"File: {e.FullPath} {e.ChangeType}");
+        }
+
+        public void OnRenamed(object sender, RenamedEventArgs re)
+        {
+            sendInfo($"File: {re.OldFullPath} renamed to {re.FullPath}");
+        }
+
+        public void sendInfo(string data)
+        {
+
+        }
+    }
+}
